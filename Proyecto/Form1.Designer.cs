422e5e56965c65ed79bd0d31701532c823f71b72
Brian Morales <briansmor09@gmail.com>
2018-03-09 18:56:22 -0600

    Funcion Abrir Archivo

--- Proyecto/Form1.Designer.cs
+++ Proyecto/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rt_entrada = new System.Windows.Forms.RichTextBox();
+            this.bt_cargar = new System.Windows.Forms.Button();
+            this.bt_analizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rt_entrada
+            // 
+            this.rt_entrada.Location = new System.Drawing.Point(32, 27);
+            this.rt_entrada.Name = "rt_entrada";
+            this.rt_entrada.Size = new System.Drawing.Size(515, 335);
+            this.rt_entrada.TabIndex = 0;
+            this.rt_entrada.Text = "";
+            // 
+            // bt_cargar
+            // 
+            this.bt_cargar.Location = new System.Drawing.Point(604, 69);
+            this.bt_cargar.Name = "bt_cargar";
+            this.bt_cargar.Size = new System.Drawing.Size(75, 23);
+            this.bt_cargar.TabIndex = 1;
+            this.bt_cargar.Text = "Cargar";
+            this.bt_cargar.UseVisualStyleBackColor = true;
+            this.bt_cargar.Click += new System.EventHandler(this.bt_cargar_Click);
+            // 
+            // bt_analizar
+            // 
+            this.bt_analizar.Location = new System.Drawing.Point(604, 118);
+            this.bt_analizar.Name = "bt_analizar";
+            this.bt_analizar.Size = new System.Drawing.Size(75, 23);
+            this.bt_analizar.TabIndex = 2;
+            this.bt_analizar.Text = "Analizar";
+            this.bt_analizar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(808, 414);
+            this.Controls.Add(this.bt_analizar);
+            this.Controls.Add(this.bt_cargar);
+            this.Controls.Add(this.rt_entrada);
+            this.Name = "Form1";
+            this.Text = "Hangman";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rt_entrada;
+        private System.Windows.Forms.Button bt_cargar;
+        private System.Windows.Forms.Button bt_analizar;
     }
 }
 
--- Proyecto/Form1.cs
+++ Proyecto/Form1.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.IO;
 namespace Proyecto
 {
     public partial class Form1 : Form
@@ -14,6 +14,17 @@ namespace Proyecto
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void bt_cargar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrirA = new OpenFileDialog();
+            abrirA.Filter="*|.lfp";
+            if(abrirA.ShowDialog()==System.Windows.Forms.DialogResult.OK){
+                StreamReader lectura = new StreamReader(abrirA.FileName);
+                String arch = lectura.ReadToEnd();
+                rt_entrada.Text = arch;
+            }
         }
     }
 }
